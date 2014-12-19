71c8d63f843dcc686057acd5ec32030252dfb4fd
Sandra Arendt <sandra@webcatgraphics.com>
2014-12-18 23:04:17 -0500

    reindeer images

---
+++ src/reindeer/ReindeerName.java
@@ -0,0 +1,16 @@
+package reindeer;
+
+public class ReindeerName
+{
+private String name;
+
+	public ReindeerName(String name)
+	{	
+		this.name = name;
+	}
+	
+	public String getValue()
+	{
+		return name;
+	}
+}
